fcc009152530b8354ac4b7859422ac6656bbbc6f
Junhan <junhan@g2.nctu.edu.tw>
2017-09-24 18:07:49 +0800

    version 3.0

--- IoTtalk_ESP12f.ino
+++ IoTtalk_ESP12f.ino
@@ -5,9 +5,9 @@
 #include "ESP8266HTTPClient.h"
 
 #define Serial Serial
-const char* ssid     = "dlink DIR-632";
-const char* password = "035731924";
-String url = "http://140.113.131.75:9999/";
+const char* ssid     = "MIRC311";//"dlink DIR-632";
+const char* password = "pcs54784";//"035731924";
+String url = "http://140.113.199.199:9999/";
 HTTPClient http;
 
 unsigned long five_min;
@@ -17,10 +17,9 @@ void setup() {
   int i;
   
   Serial.begin(115200);
-  //Serial.println("----------Start----------");
-
+  
   //connect to wifi
-  Serial.println("--Connect o Wi-Fi--------");
+  Serial.println("-----Connect to Wi-Fi-----");
   WiFi.begin(ssid, password);//進void setup 一開始就要先執行 後執行會連不上
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -29,69 +28,78 @@ void setup() {
 
   //Append the mac address to url string
   WiFi.macAddress(MAC_array);//get esp12f mac address
-  for (i=0;i<6;i++)
+  for (i=0;i<6;i++){
+    if( MAC_array[i]<0x10 )
+      url+="0";
     url+=String(MAC_array[i],HEX);
-
+  }
+  
   //send the register packet
   Serial.println("[HTTP] begin...");
   Serial.println("[HTTP] POST..." + url);
   
   http.begin(url);
+  /*
   http.setUserAgent("python-requests/2.18.1");
   http.addHeader("Connection","keep-alive");
   http.useHTTP10(true);
   http.addHeader("Accept-Encoding","gzip,deflate");
+  */
   http.addHeader("Content-Type","application/json");
-  int httpCode = http.POST("{\"profile\": {\"d_name\": \"87.Dummy_Device\", \"dm_name\": \"Dummy_Device\", \"is_sim\": false, \"df_list\": [\"Dummy_Sensor\", \"Dummy_Control\"]}}");
+  int httpCode = http.POST("{\"profile\": {\"d_name\": \"01.ESP12F\", \"dm_name\": \"ESP12F\", \"is_sim\": false, \"df_list\": [\"esp12f_LED\"]}}");
 
   Serial.println("[HTTP] POST... code:" + (String)httpCode );
   Serial.println(http.getString());
 
   http.end();
-
-  url +="/Dummy_Sensor";
+  
+  url +="/esp12f_LED";
   Serial.println("[HTTP] URL: "+url);
-  
-  Serial.println("\n[HTTP] PUT..." + url);
-  http.begin(url);
-  http.setUserAgent("python-requests/2.18.1");
-  http.addHeader("Connection","keep-alive");
-  http.useHTTP10(true);
-  http.addHeader("Accept-Encoding","gzip,deflate");
-  http.addHeader("Content-Type","application/json");
-  httpCode = http.PUT("{\"data\":["+String(random(0,10))+"]}");
-  
-  Serial.println("[HTTP] POST... code:" + (String)httpCode );
-  Serial.println(http.getString());
-
-  http.end();
-  
-  
-  
   
   five_min = millis();
 }
 
 void loop() {
   String Str_ret = http.getString();
+  int httpCode;
   
-  if(millis() - five_min >5000){
-    
+  if(millis() - five_min >10000){
+    //Serial.println("\n[HTTP] PUT..." + url);
     //http.begin(url);
-    //http.useHTTP10(1);
-    //http.addHeader("Connection","keep-alive");
-    //http.addHeader("Accept-Encoding","gzip,deflate");
-    //http.addHeader("Accept","*/*");
-    //http.setUserAgent("python-requests/2.18.1");
+    /*
+    http.setUserAgent("python-requests/2.18.1");
+    http.addHeader("Connection","keep-alive");
+    http.useHTTP10(true);
+    http.addHeader("Accept-Encoding","gzip,deflate");
+    */
     //http.addHeader("Content-Type","application/json");
+    //httpCode = http.PUT("{\"data\":["+String(random(0,10))+"]}");
     
-    //int httpCode = http.PUT("{\"data\":["+String(random(0,10))+"]}");
-  
     //Serial.println("[HTTP] POST... code:" + (String)httpCode );
     //Serial.println(http.getString());
-  
+    
     //http.end();
-    //five_min =millis();
+    
+    /////////////////////////////////////
+    delay(1000);
+    Serial.println("---------------------------------------------------");
+    /////////////////////////////////////
+    http.begin(url);
+    /*
+    http.setUserAgent("python-requests/2.18.1");
+    http.addHeader("Connection","keep-alive");
+    http.useHTTP10(true);
+    http.addHeader("Accept-Encoding","gzip,deflate");
+    */
+    http.addHeader("Content-Type","application/json");
+    httpCode = http.GET();
+    
+    Serial.println("[HTTP] GET... code:" + (String)httpCode );
+    Serial.println(http.getString());
+    
+    http.end();
+    
+    five_min =millis();
   }
   
   if (Str_ret != '\0'){
