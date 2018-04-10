ad9c383f91d488ffc5b361e8339ccf6674424822
gusony <junhan@g2.nctu.edu.tw>
2018-04-10 18:29:46 +0800

    add myesp8266 library

---
+++ MyEsp8266.h
@@ -0,0 +1,65 @@
+/*
+   my esp8266 wifi function
+*/
+#ifndef all_header
+#define all_header
+#include "ArduinoJson.h" // json library
+#include "ESP8266TrueRandom.h" // uuid library
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFiMulti.h>
+#include "ESP8266HTTPClient2.h"
+#include <EEPROM.h>
+#include <PubSubClient.h> // MQTT library
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "MyEsp8266.h"
+#endif
+
+///////////////////pin out////////////////////////////////////////////
+#define UPLOAD       0
+#define LEDPIN       2
+#define CLEAREEPROM  13 //hold for 5 second , it will erease the contain in eeprom
+
+
+#define SSD1306_IIC
+#ifdef SSD1306_IIC   //SSD1306 with IIC
+#define OLED_RESET 3 //reset pin will not be used, just for declare
+#define OLED_SDA   4 //SDA:04, SCL:05 :those two pin are on board
+#define OLED_CLK   5 //but those two define will not be used
+#endif
+//#ifdef  SSD1306_SPI  //SSD1306 with SPI
+//  #define OLED_MOSI  13   //D1 on SSD1306
+//  #define OLED_CLK   14   //D0 on SSD1306
+//  #define OLED_DC    2
+//  #define OLED_CS    15
+//  #define OLED_RESET 16
+//  Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+//#endif
+
+///////////////////pin out////////////////////////////////////////////
+
+//EEPROM
+void clr_eeprom(int sw);
+void save_WiFi_AP_Info(char *wifiSSID, char *wifiPASS, char *ServerIP);
+int  read_WiFi_AP_Info(char *wifiSSID, char *wifiPASS, char *ServerIP);
+
+//server ,ap mode
+String scan_network(void);
+void handleRoot(void);
+void handleNotFound(void);
+void start_web_server(void);
+void ap_setting(void);
+
+//switch to sta  mode
+void connect_to_wifi(char *wifiSSID, char *wifiPASS);
+void saveInfoAndConnectToWiFi(void);
+
+
+
+
+
+
+
